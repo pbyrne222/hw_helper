1f38b2397d59bcf31700ea81e5110bc4e963ae2b
Pete <petermason.byrne@gmail.com>
2014-10-24 17:55:41 -0400

    model testing

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,4 +1,8 @@
 class Problem < ActiveRecord::Base
   belongs_to :user
 	has_many :notes
+
+  validates :description, presence: true
+  validates :history, presence: true
+  validates :user, presence: true
 end
