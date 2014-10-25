6f000bea96b6de7ceeb7027d73fb4b3c2e8f5e55
DeeDee Lavinder <deedeelavinder@gmail.com>
2014-10-25 00:57:02 -0400

    tests for note model started

--- test/models/note_test.rb
+++ test/models/note_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class NoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	should validate_presence_of(:text)
+	should validate_presence_of(:user)
+	should validate_presence_of(:problem)
+
+	should belong_to(:user)
+	should belong_to(:problem)
+
+	context "a note" do
+
+		
+
+	end
 end
