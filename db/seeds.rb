72a92d71370e887ca5b640a68e27b95908098dd5
DeeDee Lavinder <deedeelavinder@gmail.com>
2014-10-25 16:46:34 -0400

    working on seed data and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,26 +11,27 @@ User.delete_all
 Problem.delete_all
 Note.delete_all
 
-user = User.create!(
+user = User.create!(name:                  "Admin",
                     email:                 "user@example.org",
                     password:              "password",
                     password_confirmation: "password")
 
-other_users = Array.new(10).map { |_| User.create!(
+other_users = Array.new(10).map { |_| User.create!(name:                  Faker::Name.name,
                                                    email:                 Faker::Internet.email,
                                                    password:              "password",
                                                    password_confirmation: "password") }
 
-20.times do
-	Problem.create!(description: "#{['In Rails, can I', 'How do you', 'Why would you'].sample} #{Faker::Company.bs}?",
-	                 history:  Faker::Hacker.say_something_smart,
-	                 user:  user)
+30.times do
+	Problem.create!(description: "#{['I keep getting stuck on the', 'Can someone explain why', 'Why would the'].sample} #{Faker::Company.catch_phrase.downcase}?",
+	                history:  Faker::Lorem.paragraph(sentence_count=6, supplemental=false, random_sentences_to_add=3),
+	                user:  user,
+									created_at: rand(1.year).ago,)
 end
 
 problems = Problem.all
 
 50.times do
 	Note.create!(problem: problems.sample,
-	               text:     Faker::Hacker.say_something_smart,
-	               user:     other_users.sample)
+	             text:     Faker::Hacker.say_something_smart,
+	             user:     other_users.sample)
 end
