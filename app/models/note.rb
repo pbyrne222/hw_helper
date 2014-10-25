c4038c178ff8ce8255790355f3565a786985df29
DeeDee Lavinder <deedeelavinder@gmail.com>
2014-10-25 13:49:54 -0400

    all tests passinggit add .git add . plus added seed data

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,8 +2,6 @@ class Note < ActiveRecord::Base
   belongs_to :problem
   belongs_to :user
 
-  before_create :authenticate_user!
-
   validates :text, presence: true
   validates :user, presence: true
   validates :problem, presence: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+User.delete_all
+Problem.delete_all
+Note.delete_all
+
+user = User.create!(
+                    email:                 "user@example.org",
+                    password:              "password",
+                    password_confirmation: "password")
+
+other_users = Array.new(10).map { |_| User.create!(
+                                                   email:                 Faker::Internet.email,
+                                                   password:              "password",
+                                                   password_confirmation: "password") }
+
+20.times do
+	Problem.create!(description: "#{['In Rails, can I', 'How do you', 'Why would you'].sample} #{Faker::Company.bs}?",
+	                 history:  Faker::Hacker.say_something_smart,
+	                 user:  user)
+end
+
+problems = Problem.all
+
+50.times do
+	Note.create!(problem: problems.sample,
+	               text:     Faker::Hacker.say_something_smart,
+	               user:     other_users.sample)
+end
